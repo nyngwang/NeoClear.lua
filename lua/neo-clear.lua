4e44df43831efda1b2654dc8b9bcd131c5d4d299
nyngwang <nyngwang@gmail.com>
2022-02-03 21:16:09 +0800

    fix noname buffer issue; also stop all timer

--- lua/neo-clear.lua
+++ lua/neo-clear.lua
@@ -2,6 +2,8 @@ M = {}
 
 
 function M.save_me()
+  if vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()) == '' then return end
+  vim.fn.timer_stopall()
   vim.cmd('noh')
   if vim.api.nvim_eval('&modified') == 1 then return end
   local topline = vim.fn.getwininfo(vim.api.nvim_get_current_win())[1].topline

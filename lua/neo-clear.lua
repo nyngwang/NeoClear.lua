e7689d89036908dacb7b2cc52e2a24244e78aa5d
nyngwang <nyngwang@gmail.com>
2022-02-05 09:17:20 +0800

    fix: noh and timer-stop(for the stupid gaming theme) should be run in any circustance

--- lua/neo-clear.lua
+++ lua/neo-clear.lua
@@ -2,10 +2,9 @@ M = {}
 
 
 function M.save_me()
-  if vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()) == '' then return end
-  vim.fn.timer_stopall()
   vim.cmd('noh')
-  if vim.api.nvim_eval('&modified') == 1 then return end
+  vim.fn.timer_stopall()
+  if vim.api.nvim_eval('&modified') == 1 or vim.fn.bufname() == '' then return end
   local topline = vim.fn.getwininfo(vim.api.nvim_get_current_win())[1].topline
   vim.cmd('e')
   vim.fn.winrestview({ topline = topline })

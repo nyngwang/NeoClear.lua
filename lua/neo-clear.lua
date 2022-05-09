8b1b152232cf0ef97009782b33b382a428f89913
nyngwang <nyngwang@gmail.com>
2022-05-10 02:32:53 +0800

    try customs

--- lua/neo-clear.lua
+++ lua/neo-clear.lua
@@ -1,5 +1,9 @@
 M = {}
+M.neo_clear_customs = {}
 
+function M.setup(opt)
+  M.neo_clear_customs = opt.custom_fn ~= nil and opt.custom_fn
+end
 
 function M.save_me()
   vim.cmd('noh')
@@ -8,6 +12,8 @@ function M.save_me()
     or vim.bo.buftype == 'nofile'
     or vim.fn.bufname() == '' then return end
   local view = vim.fn.winsaveview()
+  for _, cus_fn in ipairs(M.neo_clear_customs) do
+    cus_fn() end
   vim.cmd('e')
   vim.fn.winrestview(view)
 end

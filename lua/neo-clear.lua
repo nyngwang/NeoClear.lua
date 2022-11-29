37633db60453ba2f3bf474c9bb7634528a43973f
nyngwang <nyngwang@gmail.com>
2022-11-30 02:32:12 +0800

    not sure

--- lua/neo-clear.lua
+++ lua/neo-clear.lua
@@ -1,10 +1,8 @@
-local NOREF_NOERR_TRUNC = { noremap = true, silent = true, nowait = true }
-local NOREF_NOERR = { noremap = true, silent = true }
-local EXPR_NOREF_NOERR_TRUNC = { expr = true, noremap = true, silent = true, nowait = true }
--------------------------------------------------------------------------------------------------------
 M = {}
+-------------------------------------------------------------------------------------------------------
 local list_command_fn = {}
 local last_input = nil
+
 
 local function save_me()
   vim.cmd('noh')
@@ -15,42 +13,35 @@ local function save_me()
   local view = vim.fn.winsaveview()
   for _, cus_fn in ipairs(list_command_fn) do
     cus_fn() end
-  vim.cmd('e')
   vim.fn.winrestview(view)
+  -- fix folding https://github.com/nvim-treesitter/nvim-treesitter/issues/1424
+  if vim.fn.mode() == 'n' and vim.bo.modifiable then
+    vim.cmd([[silent! normal! >><<]])
+    vim.cmd('silent! w')
+  end
   vim.cmd("echo ''")
 end
 
--------------------------------------------------------------------------------------------------------
 
+-------------------------------------------------------------------------------------------------------
 function M.setup(opt)
   for _, cus_fn in ipairs(opt.list_command_fn) do
     table.insert(list_command_fn, cus_fn)
   end
 end
 
+
 function M.save_me()
   save_me()
 end
 
-function M.fuck()
-  local input = vim.fn.input('fuck this: ')
-  if (input == '' or input:match('%s+')) then
-    input = last_input
-  end
-  last_input = input
-  vim.cmd("exe 'normal! V'")
-  vim.cmd("/\\%V"..input)
-  vim.cmd("exe 'normal! N'")
-  vim.api.nvim_feedkeys('', 'v', true)
-  save_me()
-end
 
 local function setup_vim_commands()
   vim.cmd [[
     command! NeoClear lua require'neo-clear'.save_me()
   ]]
 end
-
 setup_vim_commands()
+
 
 return M
